5f486afcacd9391dafec0601bdbe5053ee41cd0b
Pedro Melo <pedromelo1800@gmail.com>
2022-11-13 00:28:06 -0300

    Event creation routine finalized

--- app.py
+++ app.py
@@ -2,30 +2,34 @@ from getpass import getuser
 from webbrowser import get
 from login import getUserData
 from scraper import getCourses
-from builder import validateCredentials, buildEvents
-import datetime
+from builder import validateCredentials, buildEvents, rawDateParser
+import constants
 
 #Fetch user data from server
 user = getUserData()
 process = getCourses(user)
 
 #Transform fetched data into Google Calendar API event format.
-print(process)
-
 events = []
 
 for k in process.keys():
+    parser = rawDateParser(process[k][1])
+    startString = parser.getEventStart().isoformat()
+    endString = parser.getEventEnd().isoformat()
+    frequencyRule = f'RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY={parser.getDays()};UNTIL={constants.SEMESTER_END}'
     event = {
         'summary': k,
         'location': process[k][0],
         'start':{
-
+            'dateTime': startString,
+            'timeZone': constants.TIMEZONE
         },
         'end':{
-
+            'dateTime': endString,
+            'timeZone': constants.TIMEZONE
         },
         'recurrence': [
-        'RRULE:FREQ=WEEKLY;COUNT=10'
+        frequencyRule
         ],
         'reminders': {
             'useDefault': False,
@@ -36,8 +40,9 @@ for k in process.keys():
         },
     }
 
-    print(type(process[k]))
-    print(event)
+    #print(type(process[k]))
+    #print(event)
+    events.append(event)
 
 #Validate Google Calendar API credentials and build events
 
@@ -64,7 +69,4 @@ for k in process.keys():
 #                 },
 #                 }
 
-
-
-
-#buildEvents(events,validateCredentials())
+buildEvents(events,validateCredentials())
--- builder.py
+++ builder.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 import datetime
 import os.path
 import constants
@@ -11,23 +9,95 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
+SCOPES = ['https://www.googleapis.com/auth/calendar.events.owned']
 
-def parseRawDate(rawDate):
-    days = ''
-    period = ''
-    times = ''
-    for char in rawDate:
-        if period == '':
-            if not char.isalpha():
-                days+=char
+class rawDateParser():
+    def __init__(self,rawDate):
+        self.days = ''
+        self.period = ''
+        self.times = ''
+        for char in rawDate:
+            if self.period == '':
+                if not char.isalpha():
+                    self.days+=char
+                else:
+                    self.period+=char
             else:
-                period+=char
-        else:
-            times+=char
-    return (days,period,times)
+                self.times+=char
 
-def validateCredentials():
+    def getDays(self):
+        parsedDays = []
+        for char in self.days:
+            match char:
+                case '2':
+                    parsedDays.append('MO')
+                case '3':
+                    parsedDays.append('TU')
+                case '4':
+                    parsedDays.append('WE')
+                case '5':
+                    parsedDays.append('TH')
+                case '6':
+                    parsedDays.append('FR')
+                case '7':
+                    parsedDays.append('SA')
+        
+        return ','.join(parsedDays)
+    
+    def getHour(self):
+        match self.period:
+            case 'M':
+                match self.times[0]:
+                    case '1':
+                        return 8
+                    case '3':
+                        return 10
+                    case '5':
+                        return 12
+            case 'T':
+                match self.times[0]:
+                    case '2':
+                        return 14
+                    case '4':
+                        return 16
+                    case '6':
+                        return 18
+            case 'N':
+                match self.times[0]:
+                    case '1':
+                        return 19
+                    case '3':
+                        return 20
+
+    def getEventStart(self):
+        def nextWeekday(d, weekday, hour):
+            daysAhead = weekday - d.weekday()
+            hoursAhead = hour - d.hour
+            minuteAhead = 0 - d.minute
+            secondAhead = 0 - d.second
+            msAhead = 0 - d.microsecond
+            if daysAhead <= 0: # Target day already happened this week
+                daysAhead += 7
+            if hoursAhead <= 0: # Target day already happened this week
+                hoursAhead += 24
+            if minuteAhead <= 0:
+                minuteAhead +=59
+            if secondAhead <= 0:
+                secondAhead +=60
+            if msAhead <= 0:
+                msAhead+=0
+
+            return d + datetime.timedelta(days=daysAhead,hours=hoursAhead,minutes=minuteAhead,seconds=secondAhead,microseconds=msAhead)
+
+        d = datetime.datetime.now()
+        startDay = nextWeekday(d, int(self.days[0])-2,self.getHour()-1) # 0 = Monday, 1=Tuesday, 2=Wednesday...
+        return startDay
+    
+    def getEventEnd(self):
+        return self.getEventStart() + datetime.timedelta(hours=1,minutes=55)
+
+
+def validateCredentials(): #Standalone application approach. On web view, this won't be saved whatsoever.
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -49,38 +119,16 @@ def validateCredentials():
     return creds
 
 def buildEvents(eventList,creds):
+    print('Builder starting...')
     try:
         service = build('calendar', 'v3', credentials=creds)
-        
-        for rawEvent in eventList:
-            print(rawEvent)
+        for event in eventList:
+            print(f"Event being added: {event['summary']}.")
+            print(f"    ---> starting from: {event['start']['dateTime']} ")
+            print(f"    ---> every: {event['recurrence']}")
             try:
-                event = {
-                'summary': 'Google I/O 2015',
-                'location': '800 Howard St., San Francisco, CA 94103',
-                'description': 'A chance to hear more about Google\'s developer products.',
-                'start': {
-                    'dateTime': '2015-05-28T09:00:00-07:00',
-                    'timeZone': constants.TIMEZONE,
-                },
-                'end': {
-                    'dateTime': '2015-05-28T17:00:00-07:00',
-                    'timeZone': constants.TIMEZONE,
-                },
-                'recurrence': [
-                    'RRULE:FREQ=WEEKLY;COUNT=10'
-                ],
-                'reminders': {
-                    'useDefault': False,
-                    'overrides': [
-                    {'method': 'email', 'minutes': 24 * 60},
-                    {'method': 'popup', 'minutes': 10},
-                    ],
-                },
-                }
-
-                event = service.events().insert(calendarId='primary', body=event).execute()
-                print('Event created: %s' % (event.get('htmlLink')))
+                insertedEvent = service.events().insert(calendarId='primary', body=event).execute()
+                print('Event created: %s' % (insertedEvent.get('htmlLink')))
             
             except HttpError as error:
                 print(f'An error occured: {error}')
@@ -89,7 +137,5 @@ def buildEvents(eventList,creds):
 
     except HttpError as error:
         print('An error occurred: %s' % error)
-
-
-if __name__ == '__main__':
-    main()
+    
+    print('Building finished.')
